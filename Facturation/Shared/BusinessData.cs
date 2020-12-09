03d3659cd19eb24813a80a89fdec9bd36ad51ba5
Sami DIEF <sami.dief@epsi.fr>
2020-12-09 22:45:13 +0100

    Push Initial

---
+++ Facturation/Shared/BusinessData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facturation.Shared
+{
+    public class BusinessData : IBusinessData
+    {
+        public IEnumerable<Facture> Factures { get; set; }
+
+        public BusinessData()
+        {
+            Facture factureA = new Facture(100, 50, "jean Dupont");
+            Facture factureB = new Facture(100, 75, "Patrick Bruel");
+            Facture factureC = new Facture(100, 20, "Jacques Chirac");
+
+            List<Facture> Factures = new List<Facture>();
+            Factures.Add(factureA);
+            Factures.Add(factureB);
+            Factures.Add(factureC);
+        }
+    }
+}
---
+++ Facturation/Shared/Facture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facturation.Shared
+{
+    public class Facture
+    {
+        public Double MontantTotal { get; set; }
+
+        public Double MontantDu { get; set; }
+
+        public string  Client { get; set; }
+
+        public DateTime Emission { get; set; }
+
+        public DateTime ReglementAttendu { get; set; }
+
+
+        public Facture()
+        {
+
+        }
+
+        public Facture(double total, double du, string client )
+        {
+            this.MontantTotal = total;
+            this.MontantDu = du;
+            this.Client = client;
+        }
+    }
+}
